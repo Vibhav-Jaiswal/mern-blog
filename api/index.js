eaccd42c77bf975960319acfb3b1c7d40e976715
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-02-26 12:35:36 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cookieParser from 'cookie-parser'
+import postRoutes from "./routes/post.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -23,6 +24,7 @@ app.listen(5000, () => console.log("Server is running on port 5000!!!"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 // created a middleware to handle error.
 app.use((err, req, resp, next) => {
