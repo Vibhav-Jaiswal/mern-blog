70233101fa7578197b15ac2c15406c4032f5d23d
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-02-26 17:41:00 +0530

    add posts section to the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,30 +1,33 @@
-import { useEffect, useState } from 'react';
-import {useLocation} from 'react-router-dom'
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashPosts from "../components/DashPosts";
 
 const Dashboard = () => {
   const location = useLocation();
   const [tab, setTab] = useState();
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFormUrl = urlParams.get('tab');
-    if(tabFormUrl){
-      setTab(tabFormUrl)
+    const urlParams = new URLSearchParams(location.search);
+    const tabFormUrl = urlParams.get("tab");
+    if (tabFormUrl) {
+      setTab(tabFormUrl);
     }
-  },[location.search]);
+  }, [location.search]);
 
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
-      <div className='md:w-56'>
+      <div className="md:w-56">
         {/* Sidebar */}
         <DashSidebar />
       </div>
       {/* Profile... */}
-      {tab === 'profile' && <DashProfile />}
-    </div> 
-  )
-}
+      {tab === "profile" && <DashProfile />}
+      {/* posts... */}
+      {tab === "posts" && <DashPosts />}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
